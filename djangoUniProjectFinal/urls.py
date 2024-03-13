65f7ed838ff04201c2a606bf8d22bd824fb72aea
mrme11y <45737472+mrme11y@users.noreply.github.com>
2024-03-13 22:59:14 +0000

    trying to fix favicon on branch

--- djangoUniProjectFinal/urls.py
+++ djangoUniProjectFinal/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.conf import settings
 
 from djangoUniProjectFinal import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name=''),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")), name='favicon'),
 ]
