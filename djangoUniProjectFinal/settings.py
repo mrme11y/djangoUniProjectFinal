0aebe89189391278646485c62f28d2d11aa541be
mrme11y <45737472+mrme11y@users.noreply.github.com>
2024-03-16 18:01:20 +0000

    HELPDESK - setup helpdesk app and created ticket model

--- djangoUniProjectFinal/settings.py
+++ djangoUniProjectFinal/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'helpdesk',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
---
+++ helpdesk/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Ticket(models.Model):
+    summary = models.CharField(max_length=100)
+    description = models.TextField()
+    helpdesk_agent_creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_tickets')
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_tickets', null=True, blank=True)
+    status = models.CharField(max_length=20, choices=[('New', 'New'), ('In Progress', 'In Progress'), ('Closed', 'Closed')], default='New')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.summary
