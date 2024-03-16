98d9a8f93ac42af80f81afe38cb99994bd65e7ed
mrme11y <45737472+mrme11y@users.noreply.github.com>
2024-03-16 18:34:42 +0000

    HELPDESK - refactoring to use its own helpdesk app (#22)
    * HELPDESK - setup helpdesk app and created ticket model
    
    * HELPDESK - refactoring to use its own helpdesk app
    
    * HELPDESK - testing helpdesk index updates

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .import views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('helpdesk.urls')),
+    path('', views.home, name=''),
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")), name='favicon'),
 ]
